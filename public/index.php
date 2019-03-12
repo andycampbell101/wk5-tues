d8385ac69369d12e699b0f1c00a39bddf8b51533
Andy Campbell <andrew_campbell1@hotmail.com>
2019-03-12 13:42:35 +1100

    update

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php include "templates/header.php"?>
+
+	<ul>
+		
+		<li><a href="create.php">Add an artwork</a></li>
+		<li><a href="read.php">View all</a></li>
+
+	</ul>
+
+<?php include "templates/footer.php"?>
+
---
+++ public/templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<title>Wk 4 Tuesday</title>
+	
+	<meta cahrset="utf-8">
+
+</head>
+
+<body>
+
+	<h1>Collection tracker</h1>
+
+	
